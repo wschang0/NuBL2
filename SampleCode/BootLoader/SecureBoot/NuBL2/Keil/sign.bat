bb8fccdef294c35efd3b586b01267eba433dff32
wschang0 <wschang0@nuvoton.com>
2019-10-25 13:14:13 +0800

    Update to use relative path

--- SampleCode/BootLoader/SecureBoot/NuBL2/Keil/sign.bat
+++ SampleCode/BootLoader/SecureBoot/NuBL2/Keil/sign.bat
@@ -1,4 +1,4 @@
-tfm_split d:\MCU\TF-M\trusted-firmware-m\build\app\secure_fw\tfm_s.bin d:\MCU\TF-M\trusted-firmware-m\build\app\secure_fw\tfm_s.map
+tfm_split ..\..\..\..\..\..\trusted-firmware-m\build\app\secure_fw\tfm_s.bin ..\..\..\..\..\..\trusted-firmware-m\build\app\secure_fw\tfm_s.map
 FwSign tfm_s_lr.bin+tfm_s_nsc.bin tfm_s_info.bin 0x7800
-FwSign d:\MCU\TF-M\trusted-firmware-m\build\app\tfm_ns.bin tfm_ns_info.bin 0x78C0
-@REM FwSign D:\MCU\TF-M\NuBL2\SampleCode\TF-M\tfm_ns\Nonsecure\KEIL\Objects\tfm_ns.bin tfm_ns_info.bin 0x78C0
+FwSign ..\..\..\..\..\..\trusted-firmware-m\build\app\tfm_ns.bin tfm_ns_info.bin 0x78C0
+@REM FwSign ..\..\..\..\TF-M\tfm_ns\Nonsecure\KEIL\Objects\tfm_ns.bin tfm_ns_info.bin 0x78C0
